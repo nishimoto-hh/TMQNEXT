3cb9dc5387f63102e2c08d3bf5bbc5e18aaa3e11
kato <katou.kch@om.asahi-kasei.co.jp>
2023-11-16 16:29:00 +0900

    git管理不備対応 　Dapper.OutsideSql プロジェクトのLogフォルダ配下をgit管理対象にする 　これより前の履歴からチェックアウトする場合は、このコミットの対象となるファイルを配置してリビルドすること

---
+++ TMQ6WebSolution/BackEndSolution/Dapper.OutsideSql/Log/Logger.cs
@@ -0,0 +1,50 @@
+﻿#region
+
+using System;
+using Microsoft.Extensions.Logging;
+
+#endregion
+
+namespace Jiifureit.Dapper.OutsideSql.Log
+{
+    /// <summary>
+    ///     Class for Microsoft.Extension.Logging
+    /// </summary>
+    public static class Logger
+    {
+        /// <summary>
+        ///     Category. Fully qualified class name.
+        /// </summary>
+        public static string Category { get; set; }
+
+        /// <summary>
+        ///     Log Factory.
+        /// </summary>
+        public static ILoggerFactory Factory { get; set; } = new LoggerFactory(); 
+
+
+        /// <summary>
+        ///     Create logger based on category.
+        /// </summary>
+        /// <returns></returns>
+        public static ILogger Create()
+        {
+            if (String.IsNullOrEmpty(Category))
+                Category = "System.Object";
+
+            return Factory.CreateLogger(Category);
+        }
+
+        /// <summary>
+        ///     Create logger based on T.
+        /// </summary>
+        /// <returns></returns>
+        public static ILogger<T> CreateLogger<T>()
+        {
+            if (String.IsNullOrEmpty(Category))
+                Category = "System.Object";
+
+            return Factory.CreateLogger<T>();
+        }
+    }
+}
