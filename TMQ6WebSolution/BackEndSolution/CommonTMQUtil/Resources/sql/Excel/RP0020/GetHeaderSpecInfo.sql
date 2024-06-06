3bb0b1cdeac9ba1d8c10994024cbfd709ce7e0c5
白石 雅彦 <shiraishi.mm@om.asahi-kasei.co.jp>
2024-06-06 13:51:06 +0900

    2024/06/06 帳票ー機器台帳詳細(RP0020)　で機種仕様を出力できるようにする。

--- TMQ6WebSolution/BackEndSolution/CommonTMQUtil/Resources/sql/Excel/RP0020/GetHeaderSpecInfo.sql
+++ TMQ6WebSolution/BackEndSolution/CommonTMQUtil/Resources/sql/Excel/RP0020/GetHeaderSpecInfo.sql
@@ -8,11 +8,12 @@ WITH structure_factory AS (
         structure_group_id IN (1360) 
         AND language_id = @LanguageId
 ) 
-SELECT
-    spec.spec_id
-    , CASE 
-        WHEN ex.extension_data IN ('2', '3') -- 入力形式が「2：数値」「3：数値(範囲)」の場合、仕様項目名+単位をカッコで囲んで取得する
-            THEN tra.translation_text + '(' + ( 
+, main AS ( 
+    SELECT
+        spec.spec_id
+        , ex.extension_data spec_type
+        , tra.translation_text spec_name
+        , ( 
             SELECT
                 tra.translation_text 
             FROM
@@ -29,39 +30,57 @@ SELECT
                         AND st_f.factory_id IN (0, mmsr.location_structure_id)
                 ) 
                 AND tra.structure_id = spec.spec_unit_id
-        ) + ')' 
-        ELSE tra.translation_text -- 入力形式が「1：テキスト」「4：選択」の場合は値をそのまま取得する
+        ) unit_name
+        , mmsr.display_order
+        , mmsr.machine_spec_relation_id 
+    FROM
+        ms_machine_spec_relation mmsr           -- 機種別仕様関連付マスタ
+        INNER JOIN mc_machine machine           -- 機番情報
+
+        -- 機器の工場と一致している かつ、(職種・機種大分類・機種中分類・機種小分類のいずれかと一致している または 職種がNULL)
+            ON mmsr.location_structure_id = machine.location_factory_structure_id 
+            AND ( 
+                mmsr.job_structure_id = coalesce(machine.job_kind_structure_id, 0)
+                OR mmsr.job_structure_id = coalesce(machine.job_large_classfication_structure_id, 0)
+                OR mmsr.job_structure_id = coalesce(machine.job_middle_classfication_structure_id, 0)
+                OR mmsr.job_structure_id = coalesce(machine.job_small_classfication_structure_id, 0)
+                OR mmsr.job_structure_id IS NULL
+            ) 
+
+        INNER JOIN #temp temp                   -- 一時テーブル(機器台帳一覧で選択されたレコードの機番IDが格納されている)
+            ON machine.machine_id = temp.key1 
+
+        INNER JOIN ms_spec spec                  -- 仕様項目マスタ
+            ON mmsr.spec_id = spec.spec_id
+            AND spec.delete_flg = 0 -- 削除された仕様項目は対象外
+
+        LEFT JOIN ms_translation tra            -- 翻訳マスタ
+            ON spec.translation_id = tra.translation_id 
+            AND mmsr.location_structure_id = tra.location_structure_id 
+            AND tra.language_id = @LanguageId 
+
+        LEFT JOIN ms_structure ms               -- 構成マスタ(入力形式取得用)
+            ON spec.spec_type_id = ms.structure_id 
+
+        LEFT JOIN ms_item_extension ex          -- アイテムマスタ拡張(入力形式取得用)
+            ON ms.structure_item_id = ex.item_id 
+            AND ex.sequence_no = 1 
+) 
+
+SELECT
+    main.spec_id      -- 仕様項目ID
+    , main.spec_type  -- 入力形式 「1：テキスト」「2：数値」「3：数値(範囲)」「4：選択」
+
+    -- 入力形式が「2：数値」「3：数値(範囲)」の場合かつ単位の翻訳が取得できている場合、仕様項目名+単位をカッコで囲んで取得する
+    -- ※単位の翻訳が取得できていない場合は仕様項目名のみ
+    -- 入力形式が「1：テキスト」「4：選択」の場合は値をそのまま取得する
+    , CASE 
+        WHEN main.spec_type IN ('2', '3') AND main.unit_name IS NOT NULL 
+            THEN main.spec_name + '(' + main.unit_name + ')' 
+        ELSE main.spec_name 
         END spec_name -- 仕様項目名
-     , ex.extension_data AS spec_type -- 入力形式 「1：テキスト」「2：数値」「3：数値(範囲)」「4：選択」
 FROM
-    ms_machine_spec_relation mmsr -- 機種別仕様関連付マスタ
-
-    INNER JOIN mc_machine machine -- 機番情報
-        -- 機器の職種・機種大分類・機種中分類・機種小分類のいずれかと一致している
-        ON (mmsr.job_structure_id = coalesce(machine.job_kind_structure_id, 0) or
-            mmsr.job_structure_id = coalesce(machine.job_large_classfication_structure_id, 0) or
-            mmsr.job_structure_id = coalesce(machine.job_middle_classfication_structure_id, 0) or
-            mmsr.job_structure_id = coalesce(machine.job_small_classfication_structure_id, 0))
-
-    INNER JOIN #temp temp -- 一時テーブル(機器台帳一覧で選択されたレコードの機番IDが格納されている)
-        ON machine.machine_id = temp.key1
-
-    LEFT JOIN ms_spec spec -- 仕様項目マスタ
-        ON mmsr.spec_id = spec.spec_id 
-
-    LEFT JOIN ms_translation tra -- 翻訳マスタ
-        ON spec.translation_id = tra.translation_id 
-        AND mmsr.location_structure_id = tra.location_structure_id 
-        AND tra.language_id = @LanguageId
-
-    LEFT JOIN ms_structure ms -- 構成マスタ(入力形式取得用)
-        ON spec.spec_type_id = ms.structure_id 
-
-    LEFT JOIN ms_item_extension ex -- アイテムマスタ拡張(入力形式取得用)
-        ON ms.structure_item_id = ex.item_id 
-        AND ex.sequence_no = 1 
-WHERE
-    spec.delete_flg = 0 -- 削除された仕様項目は対象外
+    main 
 ORDER BY
-    mmsr.display_order
-    , mmsr.machine_spec_relation_id
+    main.display_order
+    , main.machine_spec_relation_id
